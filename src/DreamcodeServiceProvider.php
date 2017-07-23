9e2e375446e68476b926bd47ec1659e0fe6aed20
Do Tu <tdng91@gmail.com>
2017-07-23 16:21:29 +0700

    PSR-4

---
+++ src/DreamcodeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dreamcode\Goe;
+
+use Illuminate\Support\ServiceProvider;
+
+class DreamcodeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
